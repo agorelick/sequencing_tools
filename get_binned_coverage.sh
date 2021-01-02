cd7535d761a411a75a530020da9957cafdd4e0cd
Alexander Gorelick <gorelica@mskcc.org>
2021-01-02 14:15:41 -0800

    updated binned-coverage script to use mosdepth, ran for Ozen files

--- get_binned_coverage.sh
+++ get_binned_coverage.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
 #SBATCH -J bin
-#SBATCH -c 4
+#SBATCH -c 2
 #SBATCH --error=bin.err
 #SBATCH --output=bin.out
-#SBATCH --time=1:00:00
+#SBATCH --time=2:00:00
 #SBATCH --mem-per-cpu=2G
 #SBATCH -p jgreiter
 
-module load biology
-module load py-deeptools
-cd /home/users/gorelica/box/FanDemirci_ctDNA_exosomes/Inititial_six-samples/bams/orig
+# This script invokes a software called mosdepth to calculate depth of coverage for each bam file with the given filename suffix. The options provided are for: 
+# (1) running with 4 threads
+# (2) only consider reads mapped with MAPQ = 10+ (meaning <= 10% likelihood read is incorrectly mapped)
+# (3) Not sure, run faster?
+# (4) Don't print coverage for EVERY basepair
+# (5) Get coverage in 500kb tiles
+#
+# Use bioconda to install mosdepth: conda install -c bioconda mosdepth 
+# PMID: 29096012
 
+cd /scratch/groups/jgreiter/bams/misc
 
+## Make sure bam files are indexed!
 for f in $(ls *.original.sorted.rmdup.bam | sed 's/.original.sorted.rmdup.bam//' | sort -u)
 do
-    bamCoverage --bam ${f}.original.sorted.rmdup.bam -o ${f}.original.sorted.rmdup.bindepth.bigwig --binSize 500000 --outFileFormat bedgraph
+    mosdepth -t 4 --mapq 10 --fast-mode -n --by 500000 ${f}.original.sorted.rmdup ${f}.original.sorted.rmdup.bam
 done
+
 
 
